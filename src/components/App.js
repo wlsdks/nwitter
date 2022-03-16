31d44e974cffeca6702c176bc2886056194784e3
choijinan <wlsdks12@naver.com>
2022-03-16 17:54:33 +0900

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AppRouter from "components/Router";
+import {useState} from 'react';
+import {authService} from "fbase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
